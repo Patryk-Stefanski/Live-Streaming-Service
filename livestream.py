561212356cc1f5a782d50a57594378fa8b1c338b
pstefans <patrykstefanski00@gmail.com>
2022-11-15 20:09:59 +0000

    cv2 haarcascade facial recognition working

--- livestream.py
+++ livestream.py
@@ -2,24 +2,39 @@ from flask import Flask, render_template, Response, request
 import cv2
 import time
 import numpy as np
+import pickle
 
-global rec
+global rec, net
 markFace = 0
+detectFace = 0
 face = 0
 switch = 0
 
-# Load pretrained face detection model
+# Load pretrained face detection models
 net = cv2.dnn.readNetFromCaffe('./FaceDetectionModels/deploy.prototxt.txt',
                                './FaceDetectionModels/res10_300x300_ssd_iter_140000.caffemodel')
 
-# instatiate flask app
-app = Flask(__name__, template_folder='./templates')
+face_cascade = cv2.CascadeClassifier('FaceDetectionModels/haarcascade_frontalface_default.xml')
+
+
+# instantiate flask app
+app = Flask(__name__)
+
+# Load trained face model from recognizer
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("./recognizers/face-trainner.yml")
+
+
+labels = {"person_name": 1}
+with open("labels.pickle", 'rb') as f:
+    og_labels = pickle.load(f)
+    labels = {v:k for k,v in og_labels.items()}
+
 
 camera = cv2.VideoCapture(0)
 
 
-def detect_face(frame):
-    global net
+def only_face(frame):
     (h, w) = frame.shape[:2]
     blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
                                  (300, 300), (104.0, 177.0, 123.0))
@@ -44,7 +59,6 @@ def detect_face(frame):
 
 
 def mark_face(frame):
-    global net
     (h, w) = frame.shape[:2]
     blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
                                  (300, 300), (104.0, 177.0, 123.0))
@@ -58,9 +72,9 @@ def mark_face(frame):
     box = detections[0, 0, 0, 3:7] * np.array([w, h, w, h])
     (startX, startY, endX, endY) = box.astype("int")
     try:
-        # text = "{:.2f}%".format(confidence * 100)
-        text = "W RIZZ % : " + "{:.2f}%".format(confidence * 100)
+        text = "Confidence % : " + "{:.2f}%".format(confidence * 100)
         y = startY - 10 if startY - 10 > 10 else startY + 10
+        # Color is set using BGR (Blue, Green, Red)
         cv2.rectangle(frame, (startX, startY), (endX, endY),
                       (0, 0, 255), 2)
         cv2.putText(frame, text, (startX, y),
@@ -70,15 +84,35 @@ def mark_face(frame):
     return frame
 
 
+def recognize_face(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=3)
+
+    for (x, y, w, h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+
+        id_, conf = recognizer.predict(roi_gray)
+        if conf>=75:
+            name = labels[id_]
+
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            cv2.putText(frame, name, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2, cv2.LINE_AA)
+
+    return frame
+
+
 def gen_frames():  # generate frame by frame from camera
     global out, capture, rec_frame
     while True:
         success, frame = camera.read()
         if success:
             if face:
-                frame = detect_face(frame)
+                frame = only_face(frame)
             if markFace:
                 frame = mark_face(frame)
+            if detectFace:
+                frame = recognize_face(frame)
 
             try:
                 ret, buffer = cv2.imencode('.jpg', frame)
@@ -105,6 +139,11 @@ def tasks():
             global markFace
             markFace = not markFace
             if markFace:
+                time.sleep(4)
+        elif request.form.get('detectFace') == 'Detect Face':
+            global detectFace
+            detectFace = not detectFace
+            if detectFace:
                 time.sleep(4)
 
     elif request.method == 'GET':
